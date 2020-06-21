29e6541ff8695b3bf4ff90d5b5adb4c75410e7cc
張峮瑋(Arleigh Chang) <40189046+arleigh418@users.noreply.github.com>
2020-06-21 13:30:05 +0800

    Update ops.py

--- ops.py
+++ ops.py
@@ -21,31 +21,3 @@ def read_data(input_path, debug=True):
     # trade = np.array(trade)
     # trend = np.array(trend)
     return X, y,trade,trend
-
-def train_val_test_split(X, y, is_Val ,trade, trend):
-    
-    
-    # Train set
-    X_train = X[:243, :]
-    y_train = y[:243]
-    trade_train = trade[:243]
-    trend_train = trend[:243]
-    # Test set
-    X_test = X[243:, :] 
-    y_test = y[243:]
-    trade_test = trade[243:] 
-    trend_test = trend[243:]
-
-    # Val set
-    if is_Val:
-        X_val = X[243:250, :]
-        y_val = y[243:250]
-        trade_val = trade[243:250]
-        trend_val = trend[243:250]
-    else:
-        X_val = np.zeros_like(X_test)
-        y_val = np.zeros_like(y_test)
-        trade_val = np.zeros_like(trade_test)
-        trend_val = np.zeros_like(trend_test)
-
-    return X_train, y_train, X_test, y_test, X_val, y_val,  trade_train, trend_train, trade_test, trend_test, trade_val, trend_val
